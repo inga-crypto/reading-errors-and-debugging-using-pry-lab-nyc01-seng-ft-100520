f3d3d2ca8ea772998f4548c4509d75b352b2e91e
Inga <ijaanimets@gmail.com>
2020-09-21 20:33:03 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -9,8 +9,8 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage(date)
+def engage(sdate)
   #binding.pry
-  puts state_log(star_date)
+  puts state_log(date)
   #date = generate_star_date
 end
